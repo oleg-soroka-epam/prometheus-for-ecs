5db6b8557d69b3730253eae9721b57738eddf879
Oleg Soroka <oleg_soroka@epam.com>
2023-09-13 17:54:45 +0300

    WIP

--- deploy-prometheus/env.sh
+++ deploy-prometheus/env.sh
@@ -1,11 +1,14 @@
 
 ##!/bin/bash
-export AWS_REGION=us-east-1
-export ACCOUNT_ID=123456789012
-export STACK_NAME=ecs-stack 
+export AWS_REGION=$(cat ~/.aws/config | grep '^region' | cut -d "=" -f 2); echo $AWS_REGION
+export ACCOUNT_ID=$(aws sts get-caller-identity | grep Account | grep -Eo '\d+') ; echo $ACCOUNT_ID
+export CLUSTER_NAME=$(cat ~/.aws/ecs-cluster-name); echo $CLUSTER_NAME
+export CAPACITY_PROVIDER_NAME=$(aws ecs describe-clusters --clusters "$CLUSTER_NAME" | jq -r '.clusters[].capacityProviders[]'); echo $CAPACITY_PROVIDER_NAME
+export ASG_NAME=$(aws ecs describe-capacity-providers --capacity-providers "$CAPACITY_PROVIDER_NAME" | jq -r  '.capacityProviders[].autoScalingGroupProvider.autoScalingGroupArn' | cut -d "/" -f 2); echo $ASG_NAME
+export LAUNCH_CONFIGURATION_NAME=$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names "$ASG_NAME" | jq -r '.AutoScalingGroups[].LaunchConfigurationName'); echo $LAUNCH_CONFIGURATION_NAME
+export SECURITY_GROUP_ID=$(aws autoscaling describe-launch-configurations --launch-configuration-names "$LAUNCH_CONFIGURATION_NAME" | jq -r '.LaunchConfigurations[].SecurityGroups[]'); echo $SECURITY_GROUP_ID
+export VPC_ID=$(aws ec2 describe-security-groups --group-ids "$SECURITY_GROUP_ID" | jq -r '.SecurityGroups[].VpcId'); echo $VPC_ID
+export PRIVATE_SUBNET_IDS=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$VPC_ID" "Name=tag:Tier,Values=private"  --query "Subnets[].SubnetId" --output json); echo $PRIVATE_SUBNET_IDS
+export PUBLIC_SUBNET_IDS=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$VPC_ID" "Name=tag:Tier,Values=public"  --query "Subnets[].SubnetId" --output text); echo $PUBLIC_SUBNET_IDS
 
-export CLUSTER_NAME=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query 'Stacks[0].Outputs[?OutputKey==`ClusterName`].OutputValue' --output text)
-export VPC_ID=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query 'Stacks[0].Outputs[?OutputKey==`VpcId`].OutputValue' --output text)
-export PUBLIC_SUBNET_IDS=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$VPC_ID" "Name=tag-key,Values=ecs.io/role/elb"  --query "Subnets[].SubnetId" --output text)
-export PRIVATE_SUBNET_IDS=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$VPC_ID" "Name=tag-key,Values=ecs.io/role/internal-elb"  --query "Subnets[].SubnetId" --output json)
-export SECURITY_GROUP_ID=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query 'Stacks[0].Outputs[?OutputKey==`ContainerSecurityGroup`].OutputValue' --output text)
+# export STACK_NAME=ecs-stack 
